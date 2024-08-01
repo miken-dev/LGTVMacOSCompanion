09db908d34e97053706650307bfceb909615b82e
Mike Neal <mike@miken.dev>
2024-08-01 00:49:15 -0400

    Initial Commit

---
+++ LGTVMacOSCompanion/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LGTVMacOSCompanion
+//
+//  Created by Michael Neal on 8/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LGTVMacOSCompanion/LGTVMacOSCompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  LGTVMacOSCompanionApp.swift
+//  LGTVMacOSCompanion
+//
+//  Created by Michael Neal on 8/1/24.
+//
+
+import SwiftUI
+
+@main
+struct LGTVMacOSCompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
