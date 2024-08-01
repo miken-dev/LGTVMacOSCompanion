4b1007b3470c42d6c2de682d4763e9b82097e16a
Mike Neal <mike@miken.dev>
2024-08-01 14:58:33 -0400

    add observers and listener

--- LGTVMacOSCompanion/systemEventMonitor.swift
+++ LGTVMacOSCompanion/systemEventMonitor.swift
@@ -8,7 +8,40 @@
 import AppKit
 import Foundation
 
-let notificationCenter = NSWorkspace.shared.notificationCenter
+class SystemEventMonitor {
+	private let notificationCenter = NSWorkspace.shared.notificationCenter
+	
+	func startObservers() {
+		notificationCenter.addObserver(self,
+									   selector: #selector(sleepListener(_:)),
+									   name: NSWorkspace.didWakeNotification,
+									   object: nil
+		)
+		
+		notificationCenter.addObserver(self,
+									   selector: #selector(sleepListener(_:)),
+									   name: NSWorkspace.willSleepNotification,
+									   object: nil)
+		
+	}
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		NSWorkspace.shared.notificationCenter.addObserver(self, selector: #selector(sleepListener(_:)),
+														  name: NSWorkspace.willSleepNotification, object: nil)
+		NSWorkspace.shared.notificationCenter.addObserver(self, selector: #selector(sleepListener(_:)),
+														  name: NSWorkspace.didWakeNotification, object: nil)
+	}
+	
+	
+	@objc private func sleepListener(_ Notification: Notification) {
+		print("listening to sleep")
+		if Notification.name == NSWorkspace.willSleepNotification {
+			print("Going to sleep at \(Date.now.formatted(date: .abbreviated, time: .complete))")
+		} else if Notification.name == NSWorkspace.didWakeNotification {
+			print("Woke up at \(Date.now.formatted(date: .abbreviated, time: .complete))")
+		} else {
+			print("Some other event other than the first two")
+		}
+	}
 
 	private func stopObservers() {
 		NotificationCenter.default.removeObserver(self)
