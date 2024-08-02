410db174238be0cf3e81cdecfebe2a40e12ca9a5
Mike Neal <mike@miken.dev>
2024-08-02 12:27:51 -0400

    set up testing buttons

--- LGTVMacOSCompanion/ContentView.swift
+++ LGTVMacOSCompanion/ContentView.swift
@@ -10,12 +10,26 @@ import SwiftUI
 struct ContentView: View {
 	var systemEventMonitor = SystemEventMonitor()
     var body: some View {
-		VStack {
-			Button("Start Observers") {
-				systemEventMonitor.startObservers()
+		VStack(alignment: .leading) {
+			Text("Development testing")
+				.font(.title)
+			Group {
+				Text("Observers")
+				Button("Start Observers") {
+					systemEventMonitor.startObservers()
+				}
+				Button("Restart Observers") {
+					systemEventMonitor.restartObservers()
+				}
 			}
-			Button("Restart Observers") {
-				systemEventMonitor.restartObservers()
+			Group {
+				Text("LGTV Controls")
+				Button("Turn on LGTV (WoL Packet)") { }
+				Button("Turn off LGTV") { }
+				Button("Set TV to HDMI 2") { }
+				Button("Set HDMI 2 to 'PC'") { }
+				Button("Print connected displays") { }
+				Button("Print connected USB devices") { }
 			}
 		}
         .padding()
